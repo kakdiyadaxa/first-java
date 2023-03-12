82b5694fe27e7c7a90161b016ea25ecdf7e37534
daxa <kakdiyadaxa@gmail.com>
2023-03-12 18:13:40 +0000

    commit1

---
+++ src/Calculator.java
@@ -0,0 +1,43 @@
+public class Calculator {
+  //global static/class variables
+  static int a = 10;
+  static int b = 5;
+  static int answer;
+  //main method
+    public static void main(String[] args){
+        adddition();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
